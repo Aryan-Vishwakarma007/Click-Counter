9988262d985a975af28c4b12b0fef66c08a0bf8e
Aryan-Vishwakarma007 <aryanvishwakarma1707@gmail.com>
2026-03-18 21:36:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+let count = 0;
+
+document.getElementById("decreasebtn").onclick = function(){
+    count = count - 1;
+  document.getElementById("label").innerHTML = count  ;
+}
+document.getElementById("resetbtn").onclick = function(){
+    count = 0;
+    document.getElementById("label").innerHTML = count;
+    
+}
+document.getElementById("increasebtn").onclick = function(){
+    count = count + 1;
+    document.getElementById("label").innerHTML = count;
+}
